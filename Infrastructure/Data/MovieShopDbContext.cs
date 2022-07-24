dc68c19ad597f6bd4847801bfe305b4ad69f063a
Xiaoben Yin <70068293+XiaobenYin@users.noreply.github.com>
2022-07-24 14:06:56 -0700

    added fluent API for Movie table

--- Infrastructure/Data/MovieShopDbContext.cs
+++ Infrastructure/Data/MovieShopDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationCore.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +16,47 @@ namespace Infrastructure.Data
 
         }
         public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // we can specify the fluent API way
+
+            modelBuilder.Entity<Movie>(ConfigureMovie);
+        }
+
+        private void ConfigureMovie(EntityTypeBuilder<Movie> builder)
+        {
+            // we can give the rules for our Movie entity
+            builder.HasKey(m => m.Id);  // define the primary key, and by default the Id is the primary key
+            builder.Property(m => m.Title).HasMaxLength(256);
+            builder.Property(m=> m.Overview).HasMaxLength(4096);
+            builder.Property(m => m.Tagline).HasMaxLength(512);
+            builder.Property(m => m.ImdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.TmdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.PosterUrl).HasMaxLength(2084);
+            builder.Property(m => m.BackdropUrl).HasMaxLength(2084);
+            builder.Property(m => m.OriginalLanguage).HasMaxLength(64);
+            builder.Property(m => m.UpdatedBy).HasMaxLength(512);
+            builder.Property(m => m.CreatedBy).HasMaxLength(512);
+
+            builder.Property(m => m.Price).HasColumnType("decimal(5, 2)").HasDefaultValue(9.9m);
+            builder.Property(m => m.Budget).HasColumnType("decimal(18, 4)").HasDefaultValue(9.9m);
+            builder.Property(m => m.Revenue).HasColumnType("decimal(18, 4)").HasDefaultValue(9.9m);
+            
+            // insert the default day time
+            builder.Property(m => m.CreatedDate).HasDefaultValueSql("getdate()");
+
+            // ignore the column that already exists in the db which we don't want it to be there
+            builder.Ignore(m => m.Rating);
+
+            // for the index, foreign key, columns that can be order by could also be a good idea
+            builder.HasIndex(m => m.Title);
+            builder.HasIndex(m => m.Price);
+            builder.HasIndex(m => m.Revenue);
+            builder.HasIndex(m => m.Budget);
+
+
+
+        }
     }
 }
