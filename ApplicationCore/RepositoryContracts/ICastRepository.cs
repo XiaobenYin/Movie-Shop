ecfa912ec3df6fb82b2c49680b85d430ee0936df
Xiaoben Yin <70068293+XiaobenYin@users.noreply.github.com>
2022-07-26 02:49:43 -0700

    added getbyid in cast repository

---
+++ ApplicationCore/RepositoryContracts/ICastRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.RepositoryContracts
+{
+    public class ICastRepository
+    {
+        Task<Cast> GetById(int id);
+    }
+}
